e8231fc01b0caeb4b735f40b7fcb3ba78a7b61e6
WoodExplorer <593916342@qq.com>
2017-01-20 21:44:03 +0800

    Test doc2vec with time consideration
    Sadly, when preparing train and test data with time in mind, my
    recommendation using doc2vec produced bad results.

--- try1.py
+++ try1.py
@@ -48,7 +48,7 @@ def get_least_numbers_big_data(self, alist, k):
 
     return map(lambda x:-x, max_heap)
 
-def extract_data_from_file_and_generate_train_and_test(filename, M, k, seed, delimiter, num_of_parts_of_test_data=4):
+def extract_data_from_file_and_generate_train_and_test(filename, M, k, seed, delimiter, num_of_parts_of_test_data=4, sort_by_time=False):
     test = None
     train = None
     data = {}
@@ -77,19 +77,53 @@ def extract_data_from_file_and_generate_train_and_test(filename, M, k, seed, del
             train[userId] = data[userId]
     userId = None
 
+    # sort by time: PART 1<begin>
+    print 'sort by time PART 1'
+    if sort_by_time:
+        for userId in train:
+            train[userId].sort(key=lambda x: x[2])
+        for userId in test:
+            test[userId].sort(key=lambda x: x[2])
+
+    print train[train.keys()[0]]
+    print test[test.keys()[0]]
+    raw_input()
+    # sort by time: PART 1 <end>
+
     ### split test data further
     test_real = {}
-    for k_user in test:
-        test_real[k_user] = [[], []]
-        for m, r, t in test[k_user]:
-            # every record of test dataset is supposed to be splitted into 2 parts: input part and fact/answer part
-            # How to specify the relative of these parts? 
-            #  Assign num_of_parts_of_test_data an appropriate value: each record of test dataset is supposed
-            # to constitute num_of_parts_of_test_data parts, and one of them would serve as the fact/answer part.
-            if 0 == random.randint(0, num_of_parts_of_test_data):
-                test_real[k_user][1].append((m, r, t)) # the fact/answer part in one record of test dataset
-            else:
-                test_real[k_user][0].append((m, r, t)) # the input part in one record of test dataset
+    if not sort_by_time:
+        for k_user in test:
+            test_real[k_user] = [[], []]
+            for m, r, t in test[k_user]:
+                # every record of test dataset is supposed to be splitted into 2 parts: input part and fact/answer part
+                # How to specify the relative of these parts? 
+                #  Assign num_of_parts_of_test_data an appropriate value: each record of test dataset is supposed
+                # to constitute num_of_parts_of_test_data parts, and one of them would serve as the fact/answer part.
+                if 0 == random.randint(0, num_of_parts_of_test_data):
+                    test_real[k_user][1].append((m, r, t)) # the fact/answer part in one record of test dataset
+                else:
+                    test_real[k_user][0].append((m, r, t)) # the input part in one record of test dataset
+    else: 
+        # sort by time: PART 2 <start>
+        print 'sort by time PART 2'
+        for k_user in test:
+            #print 'len(test[k_user]):', len(test[k_user])
+            #print 'num_of_parts_of_test_data:', num_of_parts_of_test_data
+            #raw_input()
+            #print (len(test[k_user]) * (1.0 / num_of_parts_of_test_data))
+            #print (int)(len(test[k_user]) * (1.0 / num_of_parts_of_test_data))
+            split_point_index = -1 * ((int)(len(test[k_user]) * (1.0 / num_of_parts_of_test_data)))
+            #print 'split_point_index:', split_point_index
+            test_real[k_user] = [test[k_user][:split_point_index], test[k_user][split_point_index:]]
+            #raw_input()
+
+        # sort by time: PART 2 <end>
+
+    print test_real[test_real.keys()[0]]
+    print 'sort_by_time:', sort_by_time
+
+    #raw_input('pause')
 
     return train, test_real
 
@@ -207,8 +241,18 @@ class RecommendatorSystem(object):
                     hit += 1
             all__for_recall += len(tu)
             all__for_precision += len(rank) #Note: In book RSP, the author used 'all += N'
-        metric_recall = hit / (all__for_recall * 1.0)
-        metric_precision = hit / (all__for_precision * 1.0)
+        
+        metric_recall = None
+        metric_precision = None
+        if 0 == all__for_recall:
+            metric_recall = 0
+        else:
+            metric_recall = hit / (all__for_recall * 1.0)
+        
+        if 0 == all__for_precision:
+            metric_precision = 0
+        else:
+            metric_precision = hit / (all__for_precision * 1.0)
 
         endtime = datetime.datetime.now()
         interval = (endtime - starttime).seconds
@@ -777,7 +821,7 @@ def main_windows():
         K = 10
         seed = 2 
         test_set_ratio = 0.2
-        train, test = extract_data_from_file_and_generate_train_and_test__with_time_consideration(data_filename, 4, 0, seed, delimiter, test_set_ratio)
+        train, test = extract_data_from_file_and_generate_train_and_test(data_filename, 4, 0, seed, delimiter, sort_by_time=True)
 
         rs = RecommendatorViaDoc2Vec()
         rs.setup({'data': train, 
